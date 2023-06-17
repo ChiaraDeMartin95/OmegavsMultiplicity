e3515519d0abb27731aa6bcb39fe31900232ba96
Chiara De Martin <mbp-cdm-01@visitor-108466360.dyndns.cern.ch>
2023-06-17 12:26:19 +0200

    fix issues

--- CompareYieldFitF.C
+++ CompareYieldFitF.C
@@ -23,8 +23,6 @@
 #include "TGraphAsymmErrors.h"
 #include "Constants.h"
 #include "ErrRatioCorr.C"
-#include </data/dataalice/AliceSoftware/aliphysics/master_chiara/src/PWG/Tools/AliPWGFunc.h>
-#include </data/dataalice/cdemart/AliPhysicsChiara/AliPhysics/PWGLF/SPECTRA/UTILS/YieldMean.C>
 
 void StyleHisto(TH1F *histo, Float_t Low, Float_t Up, Int_t color, Int_t style, TString TitleX, TString TitleY, TString title)
 {
@@ -113,11 +111,11 @@ void StylePad(TPad *pad, Float_t LMargin, Float_t RMargin, Float_t TMargin, Floa
 // take spectra in input
 // fits them to get pt-integrated yields
 
-void CompareFitF(
-    Int_t part = 5,
-    TString SysPath = "_Sel6June",
+void CompareYieldFitF(
+    Int_t part = 8,
+    TString SysPath = ""/*"_Sel6June"*/,
     TString OutputDir = "CompareFitFunctions/",
-    TString year = "LHC22m_pass4_Train79153",
+    TString year = "LHC22o_pass4_Train89684"/*"LHC22m_pass4_Train79153"*/,
     Bool_t isSysStudy = 1,
     Int_t MultType = 1, // 0: no mult for backward compatibility, 1: FT0M, 2: FV0A
     Bool_t UseTwoGauss = 0)
@@ -141,7 +139,6 @@ void CompareFitF(
   stringout += IsOneOrTwoGauss[UseTwoGauss];
   if (isSysStudy)
     stringout += SysPath;
-  stringout += "_" + nameFitFile[typefit];
   stringoutpdf = stringout;
   stringout += ".root";
   TFile *fileout = new TFile(stringout, "RECREATE");
@@ -149,6 +146,15 @@ void CompareFitF(
   gStyle->SetOptStat(0);
   gStyle->SetLegendFillColor(0);
   gStyle->SetLegendBorderSize(0);
+
+  TCanvas *canvasYield = new TCanvas("canvasYield", "canvasYield", 900, 700);
+  StyleCanvas(canvasYield, 0.05, 0.15, 0.2, 0.02);
+  TCanvas *canvasChi2 = new TCanvas("canvasChi2", "canvasChi2", 900, 700);
+  StyleCanvas(canvasChi2, 0.05, 0.15, 0.2, 0.02);
+  TCanvas *canvasTemp = new TCanvas("canvasTemp", "canvasTemp", 900, 700);
+  StyleCanvas(canvasTemp, 0.05, 0.15, 0.2, 0.02);
+  TCanvas *canvasFracExtrYield = new TCanvas("canvasFracExtrYield", "canvasFracExtrYield", 900, 700);
+  StyleCanvas(canvasFracExtrYield, 0.05, 0.15, 0.2, 0.02);
 
   TLegend *legendAllMult = new TLegend(0.22, 0.03, 0.73, 0.28);
   legendAllMult->SetHeader(SMultType[MultType] + " Multiplicity Percentile");
@@ -170,7 +176,11 @@ void CompareFitF(
   LegendPub->SetTextAlign(33);
   LegendPub->SetTextSize(0.025);
 
-  TLine *lineat1Mult = new TLine(0, 1, 8, 1);
+  TLine *lineat1Mult = new TLine(0, 1, numMult + 1, 1);
+  lineat1Mult->SetLineColor(1);
+  lineat1Mult->SetLineStyle(2);
+
+  TLine *lineat0Mult = new TLine(0, 0, numMult + 1, 0);
   lineat1Mult->SetLineColor(1);
   lineat1Mult->SetLineStyle(2);
 
@@ -178,103 +188,177 @@ void CompareFitF(
   legendfit->SetFillStyle(0);
   legendfit->SetTextSize(0.04);
 
-  TLegend *legendfitSummary = new TLegend(0.73, 0.65, 0.92, 0.75);
+  TLegend *legendfitSummary = new TLegend(0.23, 0.75, 0.42, 0.93);
   legendfitSummary->SetFillStyle(0);
   legendfitSummary->SetTextSize(0.04);
-  legendfitSummary->SetTextAlign(33);
-  legendfitSummary->AddEntry("", nameFit[typefit] + " fit", "");
+  legendfitSummary->SetTextAlign(13);
 
   // fit spectra
   Float_t LimInfYield = 0;
-  Float_t LimSupYield = 0.003;
+  Float_t LimSupYield = 0.01;
   Float_t YoffsetYield = 2;
 
-  TCanvas *canvasYield = new TCanvas("canvasYield", "canvasYield", 900, 700);
-  StyleCanvas(canvasYield, 0.05, 0.15, 0.2, 0.02);
-  TCanvas *canvasChi2 = new TCanvas("canvasChi2", "canvasChi2", 900, 700);
-  StyleCanvas(canvasChi2, 0.05, 0.15, 0.2, 0.02);
-  TCanvas *canvasTemp = new TCanvas("canvasTemp", "canvasTemp", 900, 700);
-  StyleCanvas(canvasTemp, 0.05, 0.15, 0.2, 0.02);
-  TCanvas *canvasFracExtrYield = new TCanvas("canvasFracExtrYield", "canvasFracExtrYield", 900, 700);
-  StyleCanvas(canvasFracExtrYield, 0.05, 0.15, 0.2, 0.02);
+  TH1F *hYield[numfittipo];
+  TH1F *hChi2[numfittipo];
+  TH1F *hFracExtrYield[numfittipo];
+  TH1F *hTemp[numfittipo];
+  TH1F *hYieldPubStat;
+  TH1F *hYieldPubSist;
 
-  for (Int_t f = 0; m <= numfittipo 0; m--)
+  for (Int_t f = 0; f < numfittipo; f++)
   {
+    if (f > 3)
+      continue;
     PathIn = "PtIntegratedYields/";
     PathIn += "PtIntegratedYields_" + year;
     PathIn += Spart[part];
     PathIn += IsOneOrTwoGauss[UseTwoGauss];
     if (isSysStudy)
       PathIn += SysPath;
-    PathIn += "_" + nameFitFile[typefit];
+    PathIn += "_" + nameFitFile[f];
     PathIn += ".root";
+    cout << "PathIn: " << PathIn << endl;
     fileIn = TFile::Open(PathIn, "READ");
 
     hYield[f] = (TH1F *)fileIn->Get("hYield");
     hYield[f]->SetName(Form("hYield_%d", f));
-    hChi2[f][f] = (TH1F *)fileIn->Get("hChi2[f]");
-    hChi2[f][f]->SetName(Form("hChi2[f]_%d", f));
-    hTemp[f][f] = (TH1F *)fileIn->Get("hTemp[f]");
-    hTemp[f][f]->SetName(Form("hTemp[f]_%d", f));
-    hFracExtrYield[f][f] = (TH1F *)fileIn->Get("hFracExtrYield[f]");
-    hFracExtrYield[f][f]->SetName(Form("hFracExtrYield[f]_%d", f));
+    hChi2[f] = (TH1F *)fileIn->Get("hChi2");
+    hChi2[f]->SetName(Form("hChi2_%d", f));
+    hTemp[f] = (TH1F *)fileIn->Get("hTemp");
+    hTemp[f]->SetName(Form("hTemp_%d", f));
+    hFracExtrYield[f] = (TH1F *)fileIn->Get("hFracExtrYield");
+    hFracExtrYield[f]->SetName(Form("hFracExtrYield_%d", f));
     if (f == 0)
     {
       hYieldPubStat = (TH1F *)fileIn->Get("hYieldPubStat");
       hYieldPubSist = (TH1F *)fileIn->Get("hYieldPubSist");
     }
 
-    StyleHistoYield(hYield[f], LimInfYield, LimSupYield, 1, 22, SMultType[MultType] + " Multiplicity Percentile", TitleYYieldPtInt, "", 2, 1.15, YoffsetYield);
+    StyleHistoYield(hYield[f], LimInfYield, LimSupYield, ColorFit[f], 22, SMultType[MultType] + " Multiplicity Percentile", TitleYYieldPtInt, "", 2, 1.15, YoffsetYield);
     StyleHistoYield(hYieldPubStat, LimInfYield, LimSupYield, kAzure + 7, 33, SMultType[MultType] + " Multiplicity Percentile", TitleYYieldPtInt, "", 2, 1.15, YoffsetYield);
     StyleHistoYield(hYieldPubSist, LimInfYield, LimSupYield, kAzure + 7, 33, SMultType[MultType] + " Multiplicity Percentile", TitleYYieldPtInt, "", 2, 1.15, YoffsetYield);
 
     if (f == 0)
       LegendPub->AddEntry(hYieldPubSist, "Eur.Phys.J.C 80 (2020) 167, 2020", "pl");
+    legendfitSummary->AddEntry(hYield[f], nameFit[f] + " fit", "pl");
+
     canvasYield->cd();
-    hYield[f]->Draw("e");
+    hYield[f]->Draw("same e");
     hYieldPubStat->Draw("same e0x0");
     hYieldPubSist->SetFillStyle(0);
     hYieldPubSist->Draw("same e2");
 
-    StyleHistoYield(hChi2[f], 0, 20, 1, 22, SMultType[MultType] + " Multiplicity Percentile", "Chi2/NDF", "", 2, 1.15, YoffsetYield);
+    StyleHistoYield(hChi2[f], 0, 20, ColorFit[f], 22, SMultType[MultType] + " Multiplicity Percentile", "Chi2/NDF", "", 2, 1.15, YoffsetYield);
     canvasChi2->cd();
-    hChi2[f]->Draw("e");
+    hChi2[f]->Draw("same e");
 
-    StyleHistoYield(hTemp[f], 0, 1, 1, 22, SMultType[MultType] + " Multiplicity Percentile", "T parameter", "", 2, 1.15, YoffsetYield);
+    StyleHistoYield(hTemp[f], 0, 1, ColorFit[f], 22, SMultType[MultType] + " Multiplicity Percentile", "T parameter", "", 2, 1.15, YoffsetYield);
     canvasTemp->cd();
-    hTemp[f]->Draw("e");
+    hTemp[f]->Draw("same e");
 
-    StyleHistoYield(hFracExtrYield[f], 0, 1, 1, 22, SMultType[MultType] + " Multiplicity Percentile", "FracExtrYield", "", 2, 1.15, YoffsetYield);
+    StyleHistoYield(hFracExtrYield[f], 0, 1, ColorFit[f], 22, SMultType[MultType] + " Multiplicity Percentile", "FracExtrYield", "", 2, 1.15, YoffsetYield);
     canvasFracExtrYield->cd();
-    hFracExtrYield[f]->Draw("e");
+    hFracExtrYield[f]->Draw("same e");
   }
 
   canvasYield->cd();
   LegendTitle->Draw("");
   LegendPub->Draw("");
+  legendfitSummary->Draw("same");
 
   canvasChi2->cd();
   LegendTitle->Draw("");
-  legendfitSummary->Draw("");
+  legendfitSummary->Draw("same");
 
-  canvasChi2->cd();
+  canvasTemp->cd();
   LegendTitle->Draw("");
-  legendfitSummary->Draw("");
+  legendfitSummary->Draw("same");
 
-  canvasChi2->cd();
+  canvasFracExtrYield->cd();
   LegendTitle->Draw("");
-  legendfitSummary->Draw("");
+  legendfitSummary->Draw("same");
 
-  canvasPtSpectra->SaveAs(stringoutpdf + ".pdf");
   canvasYield->SaveAs(stringoutpdf + "_YieldsvsPerc.pdf");
   canvasChi2->SaveAs(stringoutpdf + "_Chi2vsPerc.pdf");
   canvasTemp->SaveAs(stringoutpdf + "_TempvsPerc.pdf");
   canvasFracExtrYield->SaveAs(stringoutpdf + "_FracExtrYieldvsPerc.pdf");
-  canvasPtSpectra->SaveAs(stringoutpdf + ".png");
   canvasYield->SaveAs(stringoutpdf + "_YieldsvsPerc.png");
   canvasChi2->SaveAs(stringoutpdf + "_Chi2vsPerc.png");
   canvasTemp->SaveAs(stringoutpdf + "_TempvsPerc.png");
   canvasFracExtrYield->SaveAs(stringoutpdf + "_FracExtrYieldvsPerc.png");
+
+  // compute systematic uncertainty associated to choice of fit function
+  TCanvas *canvasRatioToTsallis = new TCanvas("canvasRatioToTsallis", "canvasRatioToTsallis", 800, 600);
+  StyleCanvas(canvasRatioToTsallis, 0.05, 0.15, 0.2, 0.02);
+  TCanvas *canvasNSigmaToTsallis = new TCanvas("canvasNSigmaToTsallis", "canvasNSigmaToTsallis", 800, 600);
+  StyleCanvas(canvasNSigmaToTsallis, 0.05, 0.15, 0.2, 0.02);
+  TCanvas *canvasRelUncFitChoice = new TCanvas("canvasRelUncFitChoice", "canvasRelUncFitChoice", 800, 600);
+  StyleCanvas(canvasRelUncFitChoice, 0.05, 0.15, 0.2, 0.02);
+
+  TH1F *hRatioToTsallis[numfittipo];
+  TH1F *hNSigmaToTsallis[numfittipo];
+  TH1F *hRatioToTsallisMax;
+  TH1F *hRelUncFitChoice;
+  for (Int_t f = 0; f < numfittipo; f++)
+  {
+    if (f > 3)
+      continue;
+    hRatioToTsallis[f] = (TH1F *)hYield[f]->Clone(Form("hRatioToTsallis_%d", f));
+    hNSigmaToTsallis[f] = (TH1F *)hYield[f]->Clone(Form("hNSigmaToTsallis_%d", f));
+    hRatioToTsallis[f]->Divide(hYield[3]);
+    ErrRatioCorr(hYield[f], hYield[3], hRatioToTsallis[f], 1); // I assume full correlation between the two yields
+    StyleHistoYield(hRatioToTsallis[f], 0.9, 1.1, ColorFit[f], 22, SMultType[MultType] + " Multiplicity Percentile", "Yield / Yield_{Tsallis}", "", 2, 1.15, YoffsetYield);
+    if (f == 0)
+      hRatioToTsallisMax = (TH1F *)hRatioToTsallis[f]->Clone("hRatioToTsallisMax");
+    for (Int_t i = 0; i < hRatioToTsallis[f]->GetNbinsX(); i++)
+    {
+      if (TMath::Abs(hRatioToTsallis[f]->GetBinContent(i + 1) - 1) > TMath::Abs(hRatioToTsallisMax->GetBinContent(i + 1) - 1))
+        hRatioToTsallisMax->SetBinContent(i + 1, hRatioToTsallis[f]->GetBinContent(i + 1));
+    }
+
+    canvasRatioToTsallis->cd();
+    if (f != 3)
+      hRatioToTsallis[f]->Draw("same e");
+    for (Int_t i = 0; i < hRatioToTsallis[f]->GetNbinsX(); i++)
+    {
+      hNSigmaToTsallis[f]->SetBinContent(i + 1, hRatioToTsallis[f]->GetBinContent(i + 1) / hRatioToTsallis[f]->GetBinError(i + 1));
+      hNSigmaToTsallis[f]->SetBinError(i + 1, 0);
+    }
+    StyleHistoYield(hNSigmaToTsallis[f], -10, 10, ColorFit[f], 22, SMultType[MultType] + " Multiplicity Percentile", "N_{#sigma}", "", 2, 1.15, YoffsetYield);
+    canvasNSigmaToTsallis->cd();
+    if (f != 3)
+      hNSigmaToTsallis[f]->Draw("same e");
+  }
+
+  hRelUncFitChoice = (TH1F *)hRatioToTsallisMax->Clone("hRelUncFitChoice");
+  for (Int_t b = 0; b < hRelUncFitChoice->GetNbinsX(); b++)
+  {
+    hRelUncFitChoice->SetBinContent(b + 1, 1. / 2 * TMath::Abs(1 - hRatioToTsallisMax->GetBinContent(b + 1)));
+    hRelUncFitChoice->SetBinError(b + 1, 0);
+  }
+
+  canvasRatioToTsallis->cd();
+  StyleHistoYield(hRatioToTsallisMax, 0.9, 1.1, 1, 22, SMultType[MultType] + " Multiplicity Percentile", "Yield / Yield_{Tsallis}", "", 2, 1.15, YoffsetYield);
+  LegendTitle->Draw("");
+  legendfitSummary->Draw("same");
+  lineat1Mult->Draw("same");
+  // hRatioToTsallisMax->Draw("same");
+
+  canvasNSigmaToTsallis->cd();
+  LegendTitle->Draw("");
+  legendfitSummary->Draw("same");
+  lineat0Mult->Draw("same");
+
+  canvasRelUncFitChoice->cd();
+  StyleHistoYield(hRelUncFitChoice, 0, 0.1, 1, 22, SMultType[MultType] + " Multiplicity Percentile", "Relative Uncertainty", "", 2, 1.15, YoffsetYield);
+  hRelUncFitChoice->Draw("same");
+  LegendTitle->Draw("");
+
+  canvasRatioToTsallis->SaveAs(stringoutpdf + "_RatioToTsallis.pdf");
+  canvasNSigmaToTsallis->SaveAs(stringoutpdf + "_NSigmaToTsallis.pdf");
+  canvasRelUncFitChoice->SaveAs(stringoutpdf + "_RelUncFitChoice.pdf");
+
+  hRelUncFitChoice->Write();
   fileout->Close();
 
   cout << "\nStarting from the files (for the different fit types): " << PathIn << endl;

344b64654fb15f3d26484b5f4db84b87b75581a5
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2023-08-03 18:19:42 +0200

    Use of array to simplify

--- script_RunByRunComparison.sh
+++ script_RunByRunComparison.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
-Sel="_Train100720"
-year="LHC22o_pass4_Train99659"
 SPathIn="RunByRunComparison/AnalysisResults"
 SPathInEvt="RunByRunComparison/AnalysisResultsEvts"
 
-for i in `seq 0 7` #8 runs
+#old ones (LHC22o_pass4)
+#year="LHC22o_pass4_Train99659"
+#RunN=( 528534 528531 528463 528461 528448 528381 528379 528232 )
+#CascPPTrain="_Train100720"
+
+#new ones (LHC22o_pass4_MinBias) 
+year="LHC22o_pass4_MinBias_Train108123"
+RunN=( 528531 528461 528292 527899 527895 527871 527850 527240 527109 527057 527041 526964 526641 )
+#CascPPTrain="_Train109354" #omega
+CascPPTrain="_Train109827" #xi
+
+#for i in `seq 0 7` #8 runs (LHC22o_pass4)
+for i in `seq 0 12` #12 runs (LHC22o_pass4_MinBias)
 do
-    if [ $i ==  0 ]
-    then Sel="_Train100720_Run528534" StringPathInEvt="_Run528534"
-    fi
-    if [ $i ==  1 ]
-    then Sel="_Train100720_Run528531" StringPathInEvt="_Run528531"
-    fi
-    if [ $i ==  2 ]
-    then Sel="_Train100720_Run528463" StringPathInEvt="_Run528463"
-    fi
-    if [ $i ==  3 ]
-    then Sel="_Train100720_Run528461" StringPathInEvt="_Run528461"
-    fi
-    if [ $i ==  4 ]
-    then Sel="_Train100720_Run528448" StringPathInEvt="_Run528448"
-    fi
-    if [ $i ==  5 ]
-    then Sel="_Train100720_Run528381" StringPathInEvt="_Run528381"
-    fi
-    if [ $i ==  6 ]
-    then Sel="_Train100720_Run528379" StringPathInEvt="_Run528379"
-    fi
-    if [ $i ==  7 ]
-    then Sel="_Train100720_Run528232" StringPathInEvt="_Run528232"
-    fi
-        root -l -b -q "YieldsVsPt.C(1, \"${StringPathInEvt}\", \"${Sel}\", 8, 0, 2, 1, 1, 0, \"${year}\", \"${SPathIn}\", \"${SPathInEvt}\")" #omegasum
+Sel=${CascPPTrain}"_Run"${RunN[i]}
+StringPathInEvt="_Run"${RunN[i]}
+echo ${Sel}
+echo ${StringPathInEvt}
+    #root -l -b -q "YieldsVsPt.C(1, \"${StringPathInEvt}\", \"${Sel}\", 8, 0, 2, 1, 1, 0, \"${year}\", \"${SPathIn}\", \"${SPathInEvt}\")" #omegasum
+    root -l -b -q "YieldsVsPt.C(1, \"${Sel}\", \"${StringPathInEvt}\", 5, 0, 2, 1, 1, 0, \"${year}\", \"${SPathIn}\", \"${SPathInEvt}\")" #omegasum
 done

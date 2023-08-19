d1c60257d1d3baf0d2e4d3e89be141f725ef9647
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2023-08-19 08:55:56 +0200

    Add correction for signal loss + anchoring factor + add spline to publsihed spectra and compute ratio

--- YieldEffCorr.C
+++ YieldEffCorr.C
@@ -67,6 +67,13 @@ void StyleHisto(TH1F *histo, Float_t Low, Float_t Up, Int_t color, Int_t style, 
   histo->SetTitle(title);
 }
 
+TSpline3 *sp3;
+Double_t spline(Double_t *x, Double_t *p)
+{
+  Double_t xx = x[0];
+  return sp3->Eval(xx);
+}
+
 TString titlePt = "p_{T} (GeV/c)";
 TString titleYield = "1/N_{ev} dN/dp_{T}";
 
@@ -94,6 +101,20 @@ void YieldEffCorr(Int_t part = 8,
     SPathInEff = ExtrSPathInEffXi;
   else if (part == 6 || part == 7 || part == 8)
     SPathInEff = ExtrSPathInEffOmega;
+
+  TString SfileSignalLossMB = "";
+  TString SfileSignalLoss = "";
+  if (part == 3 || part == 4 || part == 5)
+  {
+    SfileSignalLossMB = ExtrSfileSignalLossMBXi;
+    SfileSignalLoss = ExtrSfileSignalLossXi;
+  }
+  else if (part == 6 || part == 7 || part == 8)
+  {
+    SfileSignalLossMB = ExtrSfileSignalLossMBOmega;
+    SfileSignalLoss = ExtrSfileSignalLossOmega;
+  }
+
   if (mul > numMult)
   {
     cout << "Multiplciity out of range" << endl;
@@ -203,16 +224,20 @@ void YieldEffCorr(Int_t part = 8,
   Float_t RelErr = 0;
   for (Int_t i = 1; i <= histoYield->GetNbinsX(); i++)
   {
-    if (part >= 6 && part <= 8 && histoYield->GetXaxis()->GetBinLowEdge(i) < MinBinPt[part])
+    if (histoYield->GetXaxis()->GetBinLowEdge(i) < MinBinPt[part])
+    {
+      histoYieldCorr->SetBinContent(i, 0);
+      histoYieldCorr->SetBinError(i, 0);
       continue;
+    }
     cout << "\npt: " << histoYield->GetBinCenter(i) << endl;
     cout << histoYield->GetBinContent(i) << " +- " << histoYield->GetBinError(i) << endl;
     cout << histoEff->GetBinContent(histoEff->FindBin(histoYield->GetBinCenter(i))) << " +- " << histoEff->GetBinError(histoEff->FindBin(histoYield->GetBinCenter(i))) << endl;
-    cout << histoYieldCorr->GetBinContent(i) << " +- " << histoYieldCorr->GetBinError(i) << endl;
     histoYieldCorr->SetBinContent(i, histoYield->GetBinContent(i) / histoEff->GetBinContent(histoEff->FindBin(histoYield->GetBinCenter(i))));
     RelErr = sqrt(pow(histoYield->GetBinError(i) / histoYield->GetBinContent(i), 2) + pow(histoEff->GetBinError(histoEff->FindBin(histoYield->GetBinCenter(i))) / histoEff->GetBinContent(histoEff->FindBin(histoYield->GetBinCenter(i))), 2));
     // histoYieldCorr->SetBinError(i, histoYield->GetBinError(i) / histoEff->GetBinContent(histoEff->FindBin(histoYield->GetBinCenter(i))));
     histoYieldCorr->SetBinError(i, RelErr * histoYieldCorr->GetBinContent(i));
+    cout << histoYieldCorr->GetBinContent(i) << " +- " << histoYieldCorr->GetBinError(i) << endl;
   }
 
   /*
@@ -272,33 +297,152 @@ void YieldEffCorr(Int_t part = 8,
   }
   histoNorm->SetName("histoNormMB");
 
-  Float_t Err = 0;
-  Float_t ErrEL = histoNorm->GetBinError(histoNorm->FindBin(MultiplicityPerc[mul] + 0.0001)) / histoNorm->GetBinContent(histoNorm->FindBin(MultiplicityPerc[mul] + 0.0001));
-  Float_t ErrELMB = histoNormMB->GetBinError(1) / histoNormMB->GetBinContent(1);
+  Float_t RelErrSpectrum = 0;
+  Float_t EvLoss = histoNorm->GetBinContent(histoNorm->FindBin(MultiplicityPerc[mul] + 0.0001));
+  Float_t EvLossMB = histoNormMB->GetBinContent(1);
+  Float_t RelErrEL = histoNorm->GetBinError(histoNorm->FindBin(MultiplicityPerc[mul] + 0.0001)) / histoNorm->GetBinContent(histoNorm->FindBin(MultiplicityPerc[mul] + 0.0001));
+  Float_t RelErrELMB = histoNormMB->GetBinError(1) / histoNormMB->GetBinContent(1);
   for (Int_t i = 1; i <= histoYieldCorrELCorr->GetNbinsX(); i++)
   {
-    if (histoYieldCorrELCorr->GetXaxis()->GetBinLowEdge(i) < MinBinPt[part])
+    if ((histoYieldCorrELCorr->GetXaxis()->GetBinLowEdge(i)) < MinBinPt[part])
+    {
       continue;
-    Err = histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i);
+    }
+    RelErrSpectrum = histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i);
     if (isMB == 1)
     {
-      histoYieldCorrELCorr->SetBinContent(i, histoYieldCorrELCorr->GetBinContent(i) * histoNormMB->GetBinContent(1));
-      histoYieldCorrELCorr->SetBinError(i, sqrt(pow(Err, 2) + pow(ErrELMB, 2)) * histoYieldCorrELCorr->GetBinContent(i));
-      cout << "Norm factor: " << histoNormMB->GetBinContent(1) << endl;
-      cout << histoYieldCorr->GetBinContent(i) << " +- " << histoYieldCorr->GetBinError(i) << endl;
-      cout << histoYieldCorrELCorr->GetBinContent(i) << " +- " << histoYieldCorrELCorr->GetBinError(i) << endl;
-      cout << histoYieldCorrELCorr->GetBinContent(i) / histoYieldCorr->GetBinContent(i) << endl;
+      histoYieldCorrELCorr->SetBinContent(i, histoYieldCorrELCorr->GetBinContent(i) * EvLossMB);
+      histoYieldCorrELCorr->SetBinError(i, sqrt(pow(RelErrSpectrum, 2) + pow(RelErrELMB, 2)) * histoYieldCorrELCorr->GetBinContent(i));
+      cout << "\nNorm factor: " << EvLossMB << endl;
+      cout << "spectrum bef. corr: " << histoYieldCorr->GetBinContent(i) << " +- " << histoYieldCorr->GetBinError(i) << " rel error: " << histoYieldCorr->GetBinError(i) / histoYieldCorr->GetBinContent(i) << endl;
+      cout << "spectrum after corr: " << histoYieldCorrELCorr->GetBinContent(i) << " +- " << histoYieldCorrELCorr->GetBinError(i) << " rel error: " << histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i) << endl;
+      cout << "Ratio corr / uncorr: " << histoYieldCorrELCorr->GetBinContent(i) / histoYieldCorr->GetBinContent(i) << endl;
     }
     else
     {
-      histoYieldCorrELCorr->SetBinContent(i, histoYieldCorrELCorr->GetBinContent(i) * histoNorm->GetBinContent(histoNorm->FindBin(MultiplicityPerc[mul] + 0.0001)));
-      histoYieldCorrELCorr->SetBinError(i, sqrt(pow(Err, 2) + pow(ErrEL, 2)) * histoYieldCorrELCorr->GetBinContent(i));
-      cout << "Norm factor: " << histoNorm->GetBinContent(mul + 1) << endl;
-      cout << histoYieldCorrELCorr->GetBinContent(i) / histoYieldCorr->GetBinContent(i) << endl;
+      histoYieldCorrELCorr->SetBinContent(i, histoYieldCorrELCorr->GetBinContent(i) * EvLoss);
+      histoYieldCorrELCorr->SetBinError(i, sqrt(pow(RelErrSpectrum, 2) + pow(RelErrEL, 2)) * histoYieldCorrELCorr->GetBinContent(i));
+      cout << "\nNorm factor: " << EvLoss << endl;
+      cout << "Ratio corr / uncorr: " << histoYieldCorrELCorr->GetBinContent(i) / histoYieldCorr->GetBinContent(i) << endl;
     }
   }
 
   // SIGNAL LOSS CORRECTION
+  TFile *fileinSignalLossMB = new TFile(SfileSignalLossMB, "");
+  TFile *fileinSignalLoss = new TFile(SfileSignalLoss, "");
+  if (!fileinSignalLoss)
+  {
+    cout << "No signal loss file" << endl;
+    return;
+  }
+  if (!fileinSignalLossMB)
+  {
+    cout << "No signal loss file MB" << endl;
+    return;
+  }
+  TDirectory *dirSLMB = (TDirectory *)fileinSignalLossMB->Get("effSignal");
+  TH1F *histoSignalLossMB = (TH1F *)dirSLMB->Get("hEffCascSumSignal");
+  if (!histoSignalLossMB)
+  {
+    cout << "No signal loss histo MB" << endl;
+    return;
+  }
+  histoSignalLossMB->SetName("histoSignalLossMB");
+
+  TDirectory *dirSL = (TDirectory *)fileinSignalLoss->Get("SignalLossFinalHists");
+  TString histoName = "";
+  if (MultiplicityPerc[mul] < 30)
+    histoName = "hEffCascSumSignal_0";
+  else if (MultiplicityPerc[mul] < 70)
+    histoName = "hEffCascSumSignal_1";
+  else
+    histoName = "hEffCascSumSignal_2";
+  TH1F *histoSignalLoss = (TH1F *)dirSL->Get(histoName);
+  if (!histoSignalLoss)
+  {
+    cout << "No signal loss histo" << endl;
+    return;
+  }
+
+  TH1F *histoSignalLossFinal;
+  if (isMB == 1)
+    histoSignalLossFinal = (TH1F *)histoSignalLossMB->Clone("histoSignalLoss");
+  else
+    histoSignalLossFinal = (TH1F *)histoSignalLoss->Clone("histoSignalLoss");
+
+  TF1 *fitSLpol0 = new TF1("fitSLpol0", "pol0", 0, 10);
+  histoSignalLossFinal->Fit(fitSLpol0, "R+");
+  cout << "Chi2/NDF " << fitSLpol0->GetChisquare() / fitSLpol0->GetNDF() << endl;
+
+  TCanvas *cSignalLoss = new TCanvas("cSignalLoss", "cSignalLoss", 800, 600);
+  cSignalLoss->cd();
+  histoSignalLossFinal->Draw();
+
+  RelErrSpectrum = 0;
+  Float_t SigLoss = 0;
+  Float_t RelErrSigLoss = 0;
+  Float_t SigLossPol0 = 0;
+  Float_t RelErrSigLossPol0 = 0;
+  Float_t SigLossFinal = 0;
+  Float_t RelErrSigLossFinal = 0;
+  for (Int_t i = 1; i <= histoYieldCorrELCorr->GetNbinsX(); i++)
+  {
+    if (histoYieldCorrELCorr->GetXaxis()->GetBinLowEdge(i) < MinBinPt[part])
+      continue;
+    RelErrSpectrum = histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i);
+    SigLoss = histoSignalLossFinal->GetBinContent(histoSignalLossFinal->FindBin(histoYieldCorrELCorr->GetXaxis()->GetBinCenter(i)));
+    SigLossPol0 = fitSLpol0->GetParameter(0);
+    RelErrSigLoss = histoSignalLossFinal->GetBinError(histoSignalLossFinal->FindBin(histoYieldCorrELCorr->GetXaxis()->GetBinCenter(i))) / SigLoss;
+    RelErrSigLossPol0 = fitSLpol0->GetParError(0) / fitSLpol0->GetParameter(0);
+    if (isMB == 1 || MultiplicityPerc[mul] >= 70)
+    {
+      SigLossFinal = SigLoss;
+      RelErrSigLossFinal = RelErrSigLoss;
+    }
+    else
+    {
+      SigLossFinal = SigLossPol0;
+      RelErrSigLossFinal = RelErrSigLossPol0;
+    }
+    cout << "\npt interval: " << histoYieldCorrELCorr->GetXaxis()->GetBinLowEdge(i) << "-" << histoYieldCorrELCorr->GetXaxis()->GetBinUpEdge(i) << endl;
+    cout << "Signal loss factor: " << SigLoss << ", rel error: " << RelErrSigLoss << endl;
+    cout << "Signal loss from fit: " << fitSLpol0->Eval(histoYieldCorrELCorr->GetXaxis()->GetBinCenter(i)) << ", rel error: " << fitSLpol0->GetParError(0) / fitSLpol0->GetParameter(0) << endl;
+    cout << "Chosen value: " << SigLossFinal << ", rel error: " << RelErrSigLossFinal << endl;
+    cout << "Spectrum bef. SL corr: " << histoYieldCorrELCorr->GetBinContent(i) << " +- " << histoYieldCorrELCorr->GetBinError(i) << " rel error: " << histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i) << endl;
+    histoYieldCorrELCorr->SetBinContent(i, histoYieldCorrELCorr->GetBinContent(i) / SigLossFinal);
+    histoYieldCorrELCorr->SetBinError(i, sqrt(pow(RelErrSpectrum, 2) + pow(RelErrSigLoss, 2)) * histoYieldCorrELCorr->GetBinContent(i));
+    cout << "Spectrum after SL corr: " << histoYieldCorrELCorr->GetBinContent(i) << " +- " << histoYieldCorrELCorr->GetBinError(i) << " rel error: " << histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i) << endl;
+  }
+
+  // CORRECTION FOR ANCHORING - EFFICIENCY
+  // at the moment not applied
+  TFile *fileAnchFactor = new TFile(SfileinAnchoring, "");
+  if (!fileAnchFactor)
+  {
+    cout << "No anchoring file" << endl;
+    return;
+  }
+  TDirectoryFile *dirAnchFactor = (TDirectoryFile *)fileAnchFactor->Get("RatioPlot_23d1j_and_g-t");
+  dirAnchFactor->SetName("dirAnchFactor");
+  TH1F *histo23d1j = (TH1F *)dirAnchFactor->Get("23d1j");
+  TH1F *histoGt = (TH1F *)dirAnchFactor->Get("g-t");
+  TH1F *hAnchFactor = (TH1F *)histoGt->Clone("hAnchFactor");
+  hAnchFactor->Divide(histo23d1j);
+
+  TCanvas *canvasAnchFactor = new TCanvas("canvasAnchFactor", "canvasAnchFactor", 800, 600);
+  hAnchFactor->Draw();
+
+  Float_t AnchFactor = 0;
+  for (Int_t i = 1; i <= histoYieldCorrELCorr->GetNbinsX(); i++)
+  {
+    if (histoYieldCorrELCorr->GetXaxis()->GetBinLowEdge(i) < MinBinPt[part])
+      continue;
+    AnchFactor = hAnchFactor->GetBinContent(hAnchFactor->FindBin(histoYieldCorrELCorr->GetBinCenter(i)));
+    cout << "Spectrum bef. anch. factor corr: " << histoYieldCorrELCorr->GetBinContent(i) << " +- " << histoYieldCorrELCorr->GetBinError(i) << " rel error: " << histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i) << endl;
+    //histoYieldCorrELCorr->SetBinContent(i, histoYieldCorrELCorr->GetBinContent(i) * AnchFactor);
+    //histoYieldCorrELCorr->SetBinError(i, histoYieldCorrELCorr->GetBinError(i) * AnchFactor);
+    cout << "Spectrum after anch. factor corr: " << histoYieldCorrELCorr->GetBinContent(i) << " +- " << histoYieldCorrELCorr->GetBinError(i) << " rel error: " << histoYieldCorrELCorr->GetBinError(i) / histoYieldCorrELCorr->GetBinContent(i) << endl;
+  }
 
   Int_t partC = 0;
   if (part == 3 || part == 6)
@@ -320,10 +464,11 @@ void YieldEffCorr(Int_t part = 8,
   TCanvas *canvas = new TCanvas("canvas" + Spart[part], "canvas" + Spart[part], 1000, 800);
   StyleCanvas(canvas, 0.15, 0.05, 0.05, 0.15);
   canvas->cd();
-  histoYieldCorr->Draw("");
+  histoYieldCorrELCorr->Draw("");
   canvas->SaveAs(Sfileout + ".pdf");
   canvas->SaveAs(Sfileout + ".png");
 
+  // COMPARE TO PUBLISHED YIELDS
   TString SPathInPub = "PublishedYield13TeV/Results-Omega";
   TFile *fileinPub;
   TString histoPubName;
@@ -402,14 +547,62 @@ void YieldEffCorr(Int_t part = 8,
     histoPubFinal->Scale(1. / 2);
 
   StyleHisto(histoPubFinal, 0, 1.3 * histoPubFinal->GetBinContent(histoPubFinal->GetMaximumBin()), kAzure + 7, 33, TitleXPt, titleYield, "", 0, 0, 0, 1.5, 1.5, 2);
+  TSpline3 *splinePub;
+  TF1 *fsplinePub;
+  splinePub = new TSpline3(histoPubFinal, "Spline");
+  sp3 = (TSpline3 *)splinePub->Clone("SplineClone");
+  fsplinePub = new TF1("fSpline", spline, 0, 8);
 
   TCanvas *canvasComp = new TCanvas("canvasComp" + Spart[part], "canvasComp" + Spart[part], 1000, 800);
-  StyleCanvas(canvasComp, 0.15, 0.05, 0.05, 0.15);
-  canvasComp->cd();
-  histoYieldCorr->GetYaxis()->SetRangeUser(0, 1.3 * histoPubFinal->GetBinContent(histoPubFinal->GetMaximumBin()));
-  histoYieldCorr->GetXaxis()->SetRangeUser(MinBinPt[part], MaxBinPt[part]);
-  histoYieldCorr->Draw("same");
+  // StyleCanvas(canvasComp, 0.15, 0.05, 0.05, 0.15);
+  Float_t LLUpperPad = 0.33;
+  Float_t ULLowerPad = 0.33;
+  TPad *pad1 = new TPad("pad1", "pad1", 0, LLUpperPad, 1, 1); // xlow, ylow, xup, yup
+  TPad *padL1 = new TPad("padL1", "padL1", 0, 0, 1, ULLowerPad);
+
+  StylePad(pad1, 0.18, 0.01, 0.03, 0.);   // L, R, T, B
+  StylePad(padL1, 0.18, 0.01, 0.02, 0.3); // L, R, T, B
+
+  pad1->Draw();
+  pad1->cd();
+  StyleHisto(histoYieldCorrELCorr, 0, 1.3 * histoYieldCorrELCorr->GetBinContent(histoYieldCorrELCorr->GetMaximumBin()), ColorPart[partC], 33, TitleXPt, titleYield, "", 0, 0, 0, 1.5, 1.5, 2);
+  histoYieldCorrELCorr->GetYaxis()->SetRangeUser(0, 1.3 * histoPubFinal->GetBinContent(histoPubFinal->GetMaximumBin()));
+  histoYieldCorrELCorr->GetXaxis()->SetRangeUser(MinBinPt[part], 5);
+  histoYieldCorrELCorr->Draw("same");
   histoPubFinal->Draw("same");
+  fsplinePub->SetLineColor(kAzure + 7);
+  fsplinePub->SetLineWidth(2);
+  fsplinePub->Draw("same");
+
+  canvasComp->cd();
+  padL1->Draw();
+  padL1->cd();
+  TH1F *histoRatioToPub = (TH1F *)histoYieldCorrELCorr->Clone("histoRatioToPub");
+  Float_t ALow = 0;
+  Float_t AUp = 0;
+  for (Int_t b = 1; b <= histoYieldCorrELCorr->GetNbinsX(); b++)
+  {
+    ALow = histoYieldCorrELCorr->GetBinLowEdge(b);
+    AUp = histoYieldCorrELCorr->GetBinLowEdge(b + 1);
+    histoRatioToPub->SetBinContent(b, histoYieldCorrELCorr->GetBinContent(b) * histoYieldCorrELCorr->GetBinWidth(b) / fsplinePub->Integral(ALow, AUp));
+    histoRatioToPub->SetBinError(b, histoYieldCorrELCorr->GetBinError(b) * histoYieldCorrELCorr->GetBinWidth(b) / fsplinePub->Integral(ALow, AUp));
+  }
+  StyleHisto(histoRatioToPub, 0, 2, ColorPart[partC], 33, TitleXPt, "Ratio to published", "", 0, 0, 0, 1.5, 1.5, 2);
+  histoRatioToPub->GetYaxis()->SetTitleSize(0.1);
+  histoRatioToPub->GetYaxis()->SetTitleOffset(0.5);
+  histoRatioToPub->GetYaxis()->SetLabelSize(0.07);
+  histoRatioToPub->GetXaxis()->SetTitleSize(0.1);
+  histoRatioToPub->GetXaxis()->SetTitleOffset(0.9);
+  histoRatioToPub->GetXaxis()->SetLabelSize(0.07);
+  histoRatioToPub->GetYaxis()->SetRangeUser(0.2, 1.2);
+  histoRatioToPub->GetXaxis()->SetRangeUser(MinBinPt[part], 5);
+  histoRatioToPub->Draw("same");
+  TF1 *lineAt1 = new TF1("lineAt1", "1", MinBinPt[part], 5);
+  lineAt1->SetLineColor(1);
+  lineAt1->SetLineWidth(1);
+  lineAt1->SetLineStyle(7);
+  lineAt1->Draw("same");
+
   if (isMB == 1)
   {
     canvasComp->SaveAs(Sfileout + "_CompPub.pdf");
@@ -421,6 +614,10 @@ void YieldEffCorr(Int_t part = 8,
   histoYieldCorrELCorr->Write();
   fileout->Close();
 
-  cout << "\nA partire dal file: \n"
-       << SPathIn << " per lo yield raw\n e dal file: " << SPathInEffFinal << " per l'efficienza, \nho creato il file: " << Sfileout << ".pdf and .png and .root" << endl;
+  cout << "\nA partire dai file: \n"
+       << "\e[35mRaw yield: \e[0m" << SPathIn << "\n"
+       << "\e[35mEfficiency: \e[0m" << SPathInEffFinal << "\n"
+       << "\e[35mEvent correction: \e[0m" << SfileinNorm << " (for mult classes)\n"
+       << "\e[35mEvent correction: \e[0m" << SfileinNormMB << " (for MB)\n"
+       << endl;
 }

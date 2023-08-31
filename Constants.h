89c1855d79e33ccb2d18d3e7e48d2cae9d1f5d86
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2023-08-31 11:10:20 +0200

    Adapt to 900 GeV

--- Constants.h
+++ Constants.h
@@ -1,6 +1,5 @@
-const Int_t numMult = 12; // 10 // number of multiplicity sub-intervals
-// 22m_pass4_Train79153 -> numMult = 4
-// 22o_pass4_Train89684
+const Int_t numMult = 9; // number of multiplicity sub-intervals
+const Int_t numMult900GeV = 2;
 const Int_t numPart = 9; // number of particles
 const Int_t numFiles = 8;
 const Int_t numTopoVar = 17;
@@ -16,30 +15,32 @@ Int_t ColorModel[] = {kBlue +1, kGreen +2};
 
 TString EventType[3] = {"INEL", "INELgt0", "INELgt1"};
 
-Float_t MinBinPt[numPart] = {0, 0, 0, 0.4, 0.4, 0.4, 0.8, 0.8, 1.0};
+Float_t MinBinPt[numPart] = {0, 0, 0, 0.4, 0.4, 0.4, 0.8, 0.8, 0.8};
+Float_t MinBinPt900GeV[numPart] = {0, 0, 0, 0.4, 0.4, 0.4, 0.6, 0.6, 0.6};
 Float_t MaxBinPt[numPart] = {10, 10, 10, 8, 8, 8, 8, 8, 8};
-Float_t AdjustFactordNdeta = 1.81;
-// Int_t MultiplicityPerc[numMult + 1] = {0, 5, 10, 15, 20, 25, 30, 35, 40, 60, 100};
-// Int_t MultiplicityPerc[numMult + 1] = {0, 10, 20, 30, 40, 100}; 22m_pass4_Train79153
-// Float_t MultiplicityPerc[numMult + 1] = {0, 0.1, 0.5, 1, 5, 10, 15, 20, 25, 30, 35, 40, 50, 70, 100}; //22o_pass4_medium_Train89684
-Float_t MultiplicityPerc[numMult + 1] = {0, 1, 5, 10, 15, 20, 25, 30, 35, 40, 50, 70, 100};            // 22o_pass4_medium_Train89684
-Float_t dNdEtaRun2[numMult] = {26.02, 20.02, 16.17, 13.77, 12.04, 11.5, 9.5, 8.5, 7, 6.32, 4.5, 2.55}; // 22o_pass4_medium_Train89684
+Float_t AdjustFactordNdeta = 1;
+//was 1.81
+Float_t MultiplicityPerc[numMult + 1] = {0, 1, 5, 10, 20, 30, 40, 50, 70, 100}; 
+Float_t MultiplicityPerc900GeV[numMult900GeV +1] = {0, 30, 100}; 
+//Float_t dNdEtaRun2[numMult] = {26.02, 20.02, 16.17, 13.77, 12.04, 11.5, 9.5, 8.5, 7, 6.32, 4.5, 2.55}; // 22o_pass4_medium_Train89684
+Float_t dNdEtaRun2[numMult] = {26.02, 20.02, 16.17, 12.6 /*average*/, 10.5 /*average*/, 7.75 /*average*/, 6.32, 4.5, 2.55};
 Float_t dNdEtaRun2MB = 6.94;
-Float_t dNdEtaRun2ErrorL[numMult] = {0.29, 0.22, 0.18, 0.16, 0.14, 0.11, 0.11, 0.09, 0.09, 0.07, 0.05, 0.03}; // 22o_pass4_medium_Train89684
+Float_t dNdEtaRun2ErrorL[numMult] = {0.29, 0.22, 0.18, 0.15, 0.11, 0.09, 0.07, 0.05, 0.03}; // 22o_pass4_medium_Train89684
 Float_t dNdEtaRun2MBErrorL = 0.08;
-Float_t dNdEtaRun2ErrorR[numMult] = {0.35, 0.27, 0.22, 0.19, 0.17, 0.14, 0.14, 0.11, 0.11, 0.09, 0.07, 0.04}; // 22o_pass4_medium_Train89684
+Float_t dNdEtaRun2ErrorR[numMult] = {0.35, 0.27, 0.22, 0.18, 0.14, 0.11, 0.09, 0.07, 0.04}; // 22o_pass4_medium_Train89684
 Float_t dNdEtaRun2MBErrorR = 0.1;
-Float_t dNdEtaRun3[numMult] = {31.5, 26.85, 23.15, 20.67, 18.81, 17.28, 15.98, 14.82, 13.78, 12.40, 10.13, 7.37}; // Nicolò values
-//Float_t dNdEtaRun3[numMult] = {20.31, 26.85, 23.15, 20.67, 18.81, 17.28, 15.98, 14.82, 13.78, 12.40, 10.13, 3.55}; // official values
-Float_t dNdEtaRun3MB = 13.09;
-Float_t dNdEtaRun3ErrorL[numMult] = {0.29, 0.22, 0.18, 0.16, 0.14, 0.11, 0.11, 0.09, 0.09, 0.07, 0.05, 0.03}; // Run2 values
-Float_t dNdEtaRun3MBErrorL = 0.08;
-Float_t dNdEtaRun3ErrorR[numMult] = {0.35, 0.27, 0.22, 0.19, 0.17, 0.14, 0.14, 0.11, 0.11, 0.09, 0.07, 0.04}; // Run2 values
+
+//Float_t dNdEtaRun3[numMult] = {31.5, 26.85, 23.15, 20.67, 18.81, 17.28, 15.98, 14.82, 13.78, 12.40, 10.13, 7.37}; // Nicolò values
+Float_t dNdEtaRun3[numMult] = {20.04, 17.23, 14.84, 12.50, 10.30, 8.62, 7.24, 5.58, 3.52}; // official values
+Float_t dNdEtaRun3MB = 7.08;
+Float_t dNdEtaRun3ErrorL[numMult] = {0.37, 0.22, 0.19, 0.16, 0.13, 0.11, 0.09, 0.07, 0.05}; // Run3 values
+Float_t dNdEtaRun3MBErrorL = 0.09;
+Float_t dNdEtaRun3ErrorR[numMult] = {0.37, 0.22, 0.19, 0.16, 0.13, 0.11, 0.09, 0.07, 0.05}; // Run3 values
 Float_t dNdEtaRun3MBErrorR = 0.1;
+
 Int_t ColorMult[] = {634, 628, 807, kOrange - 4, 797, 815, 418, 429, 867, 856, 601, kViolet, kPink + 9, kPink + 1, 1};
 Float_t SizeMult[] = {2, 2, 2.8, 2.5, 2.8, 2, 2, 2.8, 2.5, 2.8, 2, 2, 2.8, 2.5, 2.8};
 Float_t SizeMultRatio[] = {1, 1, 1.8, 1.5, 1.8, 1, 1, 1.8, 1.5, 1.8, 1, 1, 1.8, 1.5, 1.8};
-// Int_t MarkerMult[] = {20, 21, 33, 34, 29, 24, 27, 28, 25, 25, 25, 20, 21, 33};
 Int_t MarkerMult[] = {20, 21, 33, 34, 29, 20, 21, 33, 34, 29, 20, 21, 33, 34, 29};
 Float_t ScaleFactor[] = {16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1};
 Int_t ColorMB = 1;
@@ -84,5 +85,3 @@ Float_t YieldXiMB13TeVErrStat = 0.00019;
 Float_t YieldXiMB13TeVErrSist = 0.00192;
 
 TString SSysSigExtr[] = {"", "", "_narrower1", "_wider1"};
-
-Float_t RelSystYieldTopoSel = 0.138;

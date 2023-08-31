2e41fa07ccc872b924167fa11a758c72ae2c2265
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2023-08-31 11:09:17 +0200

    Upload to Trieste server the files for fitting

--- script_UpLoadToServer.sh
+++ script_UpLoadToServer.sh
@@ -1,6 +1,9 @@
-scp Yields/YieldEffCorrLHC22m_pass4_Train79153_OmegaNeg_OneGaussFit_Mult0-10_Sel6June.root   cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
-scp Yields/YieldEffCorrLHC22m_pass4_Train79153_OmegaNeg_OneGaussFit_Mult10-20_Sel6June.root cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
-scp Yields/YieldEffCorrLHC22m_pass4_Train79153_OmegaNeg_OneGaussFit_Mult20-30_Sel6June.root cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
-scp Yields/YieldEffCorrLHC22m_pass4_Train79153_OmegaNeg_OneGaussFit_Mult30-40_Sel6June.root cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
-scp Yields/YieldEffCorrLHC22m_pass4_Train79153_OmegaNeg_OneGaussFit_Mult40-100_Sel6June.root cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
-scp Yields/YieldEffCorrLHC22m_pass4_Train79153_OmegaNeg_OneGaussFit_Mult0-100_Sel6June.root cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
+#mult=( "0.0-1.0" "1.0-5.0" "5.0-10.0" "10.0-15.0" "15.0-20.0" "20.0-25.0" "25.0-30.0" "30.0-35.0" "35.0-40.0" "40.0-50.0" "50.0-70.0" "70.0-100.0" "0-100" )
+mult=( "0.0-1.0" "1.0-5.0" "5.0-10.0" "10.0-20.0" "20.0-30.0" "30.0-40.0" "40.0-50.0" "50.0-70.0" "70.0-100.0" "0-100" )
+for i in `seq 0 11`
+#for i in 0
+do
+fileYield="Yields/YieldEffCorrLHC22o_pass4_MinBias_Train108123_Omega_BkgParab_Mult"${mult[i]}"_Train110357_INELgt0.root"
+echo ${fileYield}
+scp ${fileYield} cdemart@gr3srv.ts.infn.it://data/dataalice/cdemart/Run3Analyses/OmegavsMult/Yields/.
+done

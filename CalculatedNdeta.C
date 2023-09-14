137ec1b4dfea832117737c844485877e5225a80f
ChiaraDeMartin95 <chiara.demartin.keira@gmail.com>
2023-09-14 17:27:07 +0200

    Add dummy histogram to display dNdeta values

--- CalculatedNdeta.C
+++ CalculatedNdeta.C
@@ -24,27 +24,30 @@
 #include "Constants.h"
 #include "ErrRatioCorr.C"
 
-void CalculatedNdeta(TString PathIn = "profileFT0M.root")
+void CalculatedNdeta(TString PathIn = "hCentProfileFT0M.root"/*"profileFT0M.root"*/)
 {
 
   cout << "\nStarting from the files (for the different mult): " << PathIn << endl;
   TFile *file = new TFile(PathIn.Data());
-  TCanvas *c = (TCanvas *)file->Get("profileFT0M");
-  TProfile *hprof = (TProfile *)c->GetPrimitive("hCentProfileFT0M");
+  //TCanvas *c = (TCanvas *)file->Get("profileFT0M");
+  TProfile *hprof = (TProfile *)file->Get("hCentProfileFT0M_pub_corr");
   if (!hprof)
   {
     cout << "Error: histogram not found!" << endl;
     return;
   }
 
-  Float_t dndeta[numMult + 1];
-  Int_t counter[numMult + 1];
+  Float_t dndeta[numMult];
+  Float_t dndetaMB = 0;
+  Float_t dummy[numMult];
+  Int_t counter[numMult];
   for (int m = 0; m < numMult; m++)
   {
     if (m != numMult)
       cout << "\nBin " << MultiplicityPerc[m] << " - " << MultiplicityPerc[m + 1] << endl;
     else
       cout << "\nBin " << 0 << " - " << 100 << endl;
+    dummy[m] = 1;
     dndeta[m] = 0;
     counter[m] = 0;
     // cout << hprof->GetBinContent(1) << endl;
@@ -63,8 +66,19 @@ void CalculatedNdeta(TString PathIn = "profileFT0M.root")
 
   for (int m = 0; m < numMult; m++)
   {
-    dndeta[numMult] += dndeta[m] * (MultiplicityPerc[m + 1] - MultiplicityPerc[m]);
+    dndetaMB += dndeta[m] * (MultiplicityPerc[m + 1] - MultiplicityPerc[m]);
   }
-  dndeta[numMult] /= 100;
-  cout << "dNdetaMB = " << dndeta[numMult] << endl;
+  dndetaMB /= 100;
+  cout << "dNdetaMB = " << dndetaMB << endl;
+
+  TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+  TH1F * hDummy = new TH1F("hDummy", "hDummy", 100, 0, 20);
+  hDummy->GetXaxis()->SetTitle("dN/d#eta");
+  hDummy->GetYaxis()->SetTitle("Arbitrary value");
+  hDummy->GetYaxis()->SetRangeUser(0.9, 1.1);
+  TGraph *g = new TGraph(numMult, dndeta, dummy);
+  g->SetMarkerStyle(20);
+  g->SetMarkerSize(1.5);
+  hDummy->Draw("");
+  g->Draw("p");
 }
